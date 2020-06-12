3454245a74fd98d29101584e4ea7955684403f72
Ainur789 <48285337+Ainur789@users.noreply.github.com>
2020-06-12 16:58:55 +0600

    выполнила второе задание

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-var  money;
-var income;
-var  addExpenses;
-var deposit;
-var  mission;
-var  period;
-console.log ("I have a pen");
-alert ("Hello world");
+let  money = 60000;
+let income = 'fifteen, thousand';
+let addExpenses = 'thousand, fivehundred, twothousand';
+let deposit = true;
+let mission = 140000;
+let period = 2 ;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log("Период + равен period месяцев");
+console.log('Цель заработать + "mission"+ рублей');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+let budgetDay = money;
+console.log(budgetDay);
+
